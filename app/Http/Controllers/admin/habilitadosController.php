7268c988c883b31bfa1acb23c8645ff11aca29bf
The_Rigo <rodri.ledezma9483@gmail.com>
2020-07-03 15:30:16 -0400

    testfinal

--- app/Http/Controllers/admin/habilitadosController.php
+++ app/Http/Controllers/admin/habilitadosController.php
@@ -28,7 +28,7 @@ class habilitadosController extends Controller
 
 
       public function postulante(Combocatoria $combocatoria,$destino){
-        $postulante = Postulante::where(['convocatoria_id'=> $combocatoria->id,'item_nombre'=>$destino])->first();
+        $postulante = Postulante::where(['convocatoria_id'=> $combocatoria->id,'item_nombre'=>$destino])->get();
       
         return view('postulantes.habPostulante',compact('postulante'));
       }
